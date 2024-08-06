d4ee756e53e7b000c03dbdbcfb0dc9e4ca8ca053
Binshabs <binshabs567@gmail.com>
2024-08-06 16:05:44 +0530

    voting-component,candidate-component -implmented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CandidateComponent } from './candidate/candidate.component';
+import { VotingComponent } from './voting/voting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CandidateComponent,
+    VotingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/voting/voting.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voting',
+  templateUrl: './voting.component.html',
+  styleUrl: './voting.component.css'
+})
+export class VotingComponent {
+
+}
